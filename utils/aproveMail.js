49795df0acd98577be63ac1993134f07e4ef9a61
iturturika <iturturika89@gmail.com>
2023-03-12 15:13:46 +0200

    fix

--- utils/aproveMail.js
+++ utils/aproveMail.js
@@ -14,7 +14,7 @@ export const sendMail = ((email, token) => {
         from: 'invokergameozzy@gmail.com',
         to: email,
         subject: 'Aprove your registration on Invoker-game.com',
-        text: `Link: https://invoker-game.com/aprove-registration?token=${token}`
+        text: `Link: https://invoker-game.com/#/aprove-registration?token=${token}`
     }
 
     mail.sendMail(mailOptions, (error) => {
